f86241f5d2238250515241c949fd896984daf3c1
Rob Sweny <Robsweny@gmail.com>
2019-04-08 07:36:00 +0100

    update 2

--- app/src/main/java/com/example/udacity_6_news_app/MainActivity.java
+++ app/src/main/java/com/example/udacity_6_news_app/MainActivity.java
@@ -1,23 +1,42 @@
 package com.example.udacity_6_news_app;
 
+import android.app.Activity;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.support.design.chip.Chip;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.transition.TransitionManager;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.flexbox.FlexDirection;
+import com.google.android.flexbox.FlexboxLayout;
+import com.google.android.flexbox.JustifyContent;
+
+import java.util.ArrayList;
+
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
 public class MainActivity extends AppCompatActivity {
 
     // Declaring
-    private TextView welcome_textview;
+    private FlexboxLayout chips_layout;
     private EditText interests_edittext;
-    private Button go_button, add_button;
+    private Button go_button;
 
     // Variables
-    private int interest_counter = 0;
-    private String interest;
+    private int interest_counter = 6;
+    private String Interest;
+    private ArrayList<String> Chips = new ArrayList<>();
+    final Chip[] chip = new Chip[12];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,35 +44,206 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Initializing
-        welcome_textview = findViewById(R.id.welcome_textview);
+        final Animation animShake = AnimationUtils.loadAnimation(MainActivity.this, R.anim.shake);
+        TextView welcome_textview = findViewById(R.id.welcome_textview);
         interests_edittext = findViewById(R.id.interests_edittext);
         go_button = findViewById(R.id.go_button);
-        add_button = findViewById(R.id.add_button);
 
-        add_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Take the value from the EditText and give to the string "interest"
-                interest = String.valueOf(interests_edittext.getText()).trim();
+        chips_layout = findViewById(R.id.chips_layout);
+        chips_layout.setJustifyContent(JustifyContent.FLEX_START);
+        chips_layout.setFlexDirection(FlexDirection.ROW);
 
-                // Check if the user entered an empty value
-                if (interest.equals("")) {
-                    Toast.makeText(MainActivity.this, "Your entered interest was empty! Let's try again.", Toast.LENGTH_SHORT).show();
-                } else {
+        FlexboxLayout.LayoutParams params = new FlexboxLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
+        params.setMargins(10, 10, 10, 10);
 
-                    // Proceed to next activity
+        Chips.add("LifeStyle");
+        Chips.add("Science");
+        Chips.add("Politics");
+        Chips.add("Culture");
+        Chips.add("Tech");
+        Chips.add("Movies");
+
+        for (int i = 0; i < Chips.size(); i++) {
+            final int Index = i;
+
+            // Initialize a new chip instance
+            chip[Index] = new Chip(this, null, R.style.Widget_MaterialComponents_Chip_Entry);
+            chip[Index].setId(i);
+            chip[Index].setText(Chips.get(i));
+            chip[Index].setHeight(20);
+
+            // Change color to dark blue
+            chip[Index].setChipBackgroundColor(ColorStateList.valueOf(getResources().getColor(R.color.dark_blue)));
+            chip[Index].setTextColor(getResources().getColor(R.color.white));
+            chip[Index].setLayoutParams(params);
+            chip[Index].setPadding(3, 3, 3, 3);
+
+            // Make the chip clickable
+            chip[Index].isClickable();
+
+            chip[Index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (chip[Index].getId() == v.getId()) {
+                        //Toast.makeText(MainActivity.this, "Current Item: + " + Chips.get(Index), Toast.LENGTH_SHORT).show();
+                        if (chip[Index].getChipIcon() == null) {
+                            chip[Index].setChipIcon(getResources().getDrawable(R.drawable.ic_close_12dp));
+                            interest_counter--;
+
+                            if (interest_counter == 0) {
+                                go_button.setEnabled(false);
+                            } // if(interest_counter == 0)
+
+                        } else {
+                            chip[Index].setChipIcon(null);
+                            interest_counter++;
+                            go_button.setEnabled(true);
+                        }
+                    }
                 }
-            } // End onClick
-        }); // End go_button onClick
+            });
+
+            // Set chip close icon click listener
+            chip[Index].setOnCloseIconClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Smoothly remove chip from chip group
+                    TransitionManager.beginDelayedTransition(chips_layout);
+                    chips_layout.removeView(chip[Index]);
+                }
+            });
+
+            // Finally, add the chip to chip group
+            chips_layout.addView(chip[Index]);
+        }
 
         go_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Go to next activity
+                // Take the value from the EditText and give to the string "interest"
+                if (interest_counter > 11) {
+                    hideEverything(view);
+                    Toast.makeText(MainActivity.this, "Whoa whoa!\n Too many interests", Toast.LENGTH_SHORT).show();
+                } else {
+                    Interest = String.valueOf(interests_edittext.getText()).trim();
+                    addChip(Interest);
+                    interest_counter++;
+                }
 
+                hideEverything(view);
             } // End onClick
         }); // End go_button onClick
 
 
+        // When the user clicks off the EditText, hide the keyboard
+        interests_edittext.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(view);
+                }
+            }
+        });
+
+        // Listen to the EditText
+        interests_edittext.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Give the EditText a shake to prompt the users attention
+                if (s.toString().trim().length() == 15) {
+                    interests_edittext.startAnimation(animShake);
+                    Toast.makeText(MainActivity.this, "Maximum characters has been reached!", Toast.LENGTH_SHORT).show();
+                }
+
+                // If edit text is empty hide add_button
+                if (s.toString().trim().length() == 0 && interest_counter == 0) {
+                    go_button.setEnabled(false);
+                    go_button.setText(R.string.go_button_text);
+                } else {
+                    go_button.setEnabled(true);
+                    go_button.setText(R.string.add_button_text);
+                }
+            } // End onTextChanged
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+        });
     } // End onCreate
+
+    public void addChip(String Interest) {
+        Chips.add(Interest);
+        final int Index = Chips.size();
+
+        // Initialize a new chip instance
+        chip[Index] = new Chip(this, null, R.style.Widget_MaterialComponents_Chip_Entry);
+        chip[Index].setId(Index);
+        chip[Index].setText(Interest);
+        chip[Index].setHeight(20);
+
+        // Change color to dark blue
+        chip[Index].setChipBackgroundColor(ColorStateList.valueOf(getResources().getColor(R.color.dark_blue)));
+        chip[Index].setTextColor(getResources().getColor(R.color.white));
+
+        FlexboxLayout.LayoutParams params = new FlexboxLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
+        params.setMargins(10, 10, 10, 10);
+        chip[Index].setLayoutParams(params);
+        chip[Index].setPadding(3, 3, 3, 3);
+
+        // Make the chip clickable
+        chip[Index].isClickable();
+
+        chip[Index].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (chip[Index].getId() == v.getId()) {
+                    //Toast.makeText(MainActivity.this, "Current Item: + " + Chips.get(Index), Toast.LENGTH_SHORT).show();
+                    if (chip[Index].getChipIcon() == null) {
+                        chip[Index].setChipIcon(getResources().getDrawable(R.drawable.ic_close_12dp));
+                        interest_counter--;
+
+                        if (interest_counter == 0) {
+                            go_button.setEnabled(false);
+                        } // if(interest_counter == 0)
+
+                    } else {
+                        chip[Index].setChipIcon(null);
+                        interest_counter++;
+                        go_button.setEnabled(true);
+                    }
+                }
+            }
+        });
+
+        // Set chip close icon click listener
+        chip[Index].setOnCloseIconClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Smoothly remove chip from chip group
+                TransitionManager.beginDelayedTransition(chips_layout);
+                chips_layout.removeView(chip[Index]);
+            }
+        });
+
+        // Finally, add the chip to chip group
+        chips_layout.addView(chip[Index]);
+    } // End add chip
+
+    public void hideEverything(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        // Clear text for next
+        interests_edittext.setText(null);
+        interests_edittext.startAnimation((Animation) getResources().getAnimation(R.anim.shake));
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
 } // End MainActivity
